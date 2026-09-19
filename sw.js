19702cfb855db18b404cb1a231065b1361ad7404
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-19 03:29:05 +0100

    Simplify Android PWA service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="dexter-ai-shell-v2";
-const SHELL=["./","./index.html","./manifest.webmanifest","./app-icon.svg","./icon-192.png","./icon-512.png","./pwa.js"];
+const CACHE="dexter-ai-shell-v3";
+const SHELL=["./","./index.html","./manifest.json","./pwa.js","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
@@ -7,13 +7,14 @@ self.addEventListener("activate",event=>{
   event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener("fetch",event=>{
-  const req=event.request;
-  if(req.method!=="GET")return;
-  const url=new URL(req.url);
-  if(url.origin!==location.origin)return;
-  event.respondWith(fetch(req).then(res=>{
-    const copy=res.clone();
-    caches.open(CACHE).then(cache=>cache.put(req,copy)).catch(()=>{});
-    return res;
-  }).catch(()=>caches.match(req).then(r=>r||caches.match("./index.html"))));
+  if(event.request.method!=="GET")return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  event.respondWith(fetch(event.request).then(response=>{
+    if(response && response.ok){
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+    }
+    return response;
+  }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match("./index.html"))));
 });
