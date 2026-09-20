bec0b68174cf307dc1c037f07b921f168497cdf2
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-20 19:21:30 +0100

    Bump advanced Dexter PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="dexter-ai-test-v4";
+const CACHE="dexter-ai-test-v5";
 const SHELL=["./","./index.html","./manifest.webmanifest","./app-icon.svg","./upgrade.js","./advanced.js"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
