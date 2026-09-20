0cf72e0dec5c29c8d7d200ebbb45f54be47cd313
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-20 22:46:39 +0100

    Fix provisioning policy compliance result

--- android-app/app/src/main/java/uk/co/dextersspot/dexterai/PolicyComplianceActivity.java
+++ android-app/app/src/main/java/uk/co/dextersspot/dexterai/PolicyComplianceActivity.java
@@ -1,15 +1,16 @@
 package uk.co.dextersspot.dexterai;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class PolicyComplianceActivity extends Activity {
     @Override
     protected void onCreate(Bundle state) {
         super.onCreate(state);
-        DeviceOwnerPolicy.applySafeDefaults(this);
-        DeviceAgentService.start(this);
-        setResult(RESULT_OK);
+
+        Intent result = new Intent();
+        setResult(RESULT_OK, result);
         finish();
     }
 }
