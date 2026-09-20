1014889e8530d67ac42eb0397f18fbc09a074a1b
Dexter AI Test Publisher <actions@users.noreply.github.com>
2026-09-20 18:04:08 +0000

    Publish canonical Dexter AI test build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="dexter-ai-test-v3";
+const CACHE="dexter-ai-test-v4";
 const SHELL=["./","./index.html","./manifest.webmanifest","./app-icon.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
