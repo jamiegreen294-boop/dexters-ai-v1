6f4c041b59743a4e376ac13d71b3e8cceece2d09
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-20 19:11:59 +0100

    Wire advanced Dexter AI test UI into PWA publish

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE="dexter-ai-test-v4";
-const SHELL=["./","./index.html","./manifest.webmanifest","./app-icon.svg"];
+const SHELL=["./","./index.html","./manifest.webmanifest","./app-icon.svg","./upgrade.js","./advanced.js"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
