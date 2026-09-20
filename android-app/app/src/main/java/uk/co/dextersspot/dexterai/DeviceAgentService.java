8bd9df3b914159a0daec0788c4f51d7d4750b1d3
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-20 19:40:42 +0100

    Keep phone agent compatible without AndroidX

--- android-app/app/src/main/java/uk/co/dextersspot/dexterai/DeviceAgentService.java
+++ android-app/app/src/main/java/uk/co/dextersspot/dexterai/DeviceAgentService.java
@@ -6,7 +6,6 @@ import android.content.pm.*;
 import android.net.Uri;
 import android.os.*;
 import android.provider.Settings;
-import androidx.annotation.Nullable;
 import org.json.*;
 import java.io.*;
 import java.net.*;
