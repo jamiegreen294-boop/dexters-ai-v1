692fb49f69474f18016eea615adbcde7828e99a8
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-22 16:04:39 +0100

    Complete SmsReceiver.java

--- dexter-os/oem/aosp/apps/DexterMessaging/src/uk/co/dexter/os/messaging/SmsReceiver.java
+++ dexter-os/oem/aosp/apps/DexterMessaging/src/uk/co/dexter/os/messaging/SmsReceiver.java
@@ -1,3 +1,24 @@
 package uk.co.dexter.os.messaging;
+
 import android.content.*;
-public class SmsReceiver extends BroadcastReceiver{public void onReceive(Context c,Intent i){/* OEM build stores/indexes incoming SMS in Dexter's message provider. */}}
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import java.util.*;
+
+public class SmsReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context c,Intent i) {
+        try {
+            SmsMessage[] messages=Telephony.Sms.Intents.getMessagesFromIntent(i);
+            if(messages==null)return;
+            for(SmsMessage m:messages) {
+                ContentValues v=new ContentValues();
+                v.put(Telephony.Sms.ADDRESS,m.getOriginatingAddress());
+                v.put(Telephony.Sms.BODY,m.getMessageBody());
+                v.put(Telephony.Sms.DATE,m.getTimestampMillis());
+                v.put(Telephony.Sms.READ,0);
+                v.put(Telephony.Sms.SEEN,0);
+                c.getContentResolver().insert(Telephony.Sms.Inbox.CONTENT_URI,v);
+            }
+        } catch(Exception ignored) {}
+    }
+}
