061d6bf788c3d9e551b24700c6038077da8d5b0f
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-22 11:10:22 +0100

    OEM integration: dexter_os_common.mk

---
+++ dexter-os/oem/aosp/dexter_os_common.mk
@@ -0,0 +1,17 @@
+# Common Dexter OS product integration.
+PRODUCT_PACKAGES += DexterOS
+
+PRODUCT_COPY_FILES += \
+    vendor/dexter/permissions/privapp-permissions-dexter.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-dexter.xml \
+    vendor/dexter/permissions/default-permissions-dexter.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions-dexter.xml
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.dexter.os=true \
+    ro.dexter.edition=commercial \
+    ro.dexter.channel=stable \
+    ro.setupwizard.mode=DISABLED
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.locale=en-GB
+
+# Device-specific products should inherit this file after their vendor/device product.
