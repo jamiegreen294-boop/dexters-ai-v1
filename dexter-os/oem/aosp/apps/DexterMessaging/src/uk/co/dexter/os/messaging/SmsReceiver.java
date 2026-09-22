a091edfdb0047c346dad15f7fabf29173c6baa77
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-22 15:03:20 +0100

    Add messaging/SmsReceiver.java

---
+++ dexter-os/oem/aosp/apps/DexterMessaging/src/uk/co/dexter/os/messaging/SmsReceiver.java
@@ -0,0 +1,3 @@
+package uk.co.dexter.os.messaging;
+import android.content.*;
+public class SmsReceiver extends BroadcastReceiver{public void onReceive(Context c,Intent i){/* OEM build stores/indexes incoming SMS in Dexter's message provider. */}}
