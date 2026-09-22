afac3fe96f003c56ad73f244d6f1ec5e8105cb6a
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-22 10:46:29 +0100

    Hide Android system UI during Dexter boot splash

--- android-app/app/src/main/java/uk/co/dextersspot/dexterai/DexterBootSplashActivity.java
+++ android-app/app/src/main/java/uk/co/dextersspot/dexterai/DexterBootSplashActivity.java
@@ -19,6 +19,7 @@ public class DexterBootSplashActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setStatusBarColor(Color.BLACK);
         getWindow().setNavigationBarColor(Color.BLACK);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
         FrameLayout shell=new FrameLayout(this);
         shell.setBackgroundColor(Color.BLACK);
