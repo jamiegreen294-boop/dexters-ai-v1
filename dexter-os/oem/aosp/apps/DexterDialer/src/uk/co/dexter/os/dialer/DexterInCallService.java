3b51ddb7c61ae567c5ae72e0b38b3de5f6fa0444
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-22 16:04:08 +0100

    Complete DexterInCallService.java

--- dexter-os/oem/aosp/apps/DexterDialer/src/uk/co/dexter/os/dialer/DexterInCallService.java
+++ dexter-os/oem/aosp/apps/DexterDialer/src/uk/co/dexter/os/dialer/DexterInCallService.java
@@ -1,7 +1,43 @@
 package uk.co.dexter.os.dialer;
-import android.content.*;import android.telecom.*;
-public class DexterInCallService extends InCallService{
- static volatile Call current;
- public void onCallAdded(Call c){super.onCallAdded(c);current=c;Intent i=new Intent(this,InCallActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);startActivity(i);}
- public void onCallRemoved(Call c){if(current==c)current=null;super.onCallRemoved(c);}
+
+import android.content.*;
+import android.telecom.*;
+
+public class DexterInCallService extends InCallService {
+    static volatile Call current;
+    static volatile DexterInCallService instance;
+
+    @Override public void onCreate() {
+        super.onCreate();
+        instance=this;
+    }
+
+    @Override public void onDestroy() {
+        if(instance==this) instance=null;
+        super.onDestroy();
+    }
+
+    @Override public void onCallAdded(Call c) {
+        super.onCallAdded(c);
+        current=c;
+        Intent i=new Intent(this,InCallActivity.class)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(i);
+    }
+
+    @Override public void onCallRemoved(Call c) {
+        if(current==c) current=null;
+        super.onCallRemoved(c);
+    }
+
+    static void mute(boolean value) {
+        DexterInCallService s=instance;
+        if(s!=null) s.setMuted(value);
+    }
+
+    @SuppressWarnings("deprecation")
+    static void speaker(boolean value) {
+        DexterInCallService s=instance;
+        if(s!=null) s.setAudioRoute(value ? CallAudioState.ROUTE_SPEAKER : CallAudioState.ROUTE_EARPIECE);
+    }
 }
