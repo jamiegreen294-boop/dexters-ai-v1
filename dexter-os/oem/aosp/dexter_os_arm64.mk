4882ffa0550c5dc69ef40788754c1401ef6eac86
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-22 16:02:46 +0100

    Make generic arm64 product inherit Dexter common

--- dexter-os/oem/aosp/dexter_os_arm64.mk
+++ dexter-os/oem/aosp/dexter_os_arm64.mk
@@ -1,8 +1,10 @@
 # Dexter OS generic arm64 product foundation.
-# A real handset product should inherit this file from its device-specific product.
+# This target is for common-product validation only.
+# A retail handset must inherit dexter_os_common.mk from its device-specific product.
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, vendor/dexter/dexter_os_common.mk)
 
 PRODUCT_NAME := dexter_os_arm64
 PRODUCT_DEVICE := dexter
@@ -16,14 +18,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.dexter.edition=commercial \
     ro.product.locale=en-GB
 
-PRODUCT_PACKAGES += \
-    DexterLauncher \
-    DexterSettings \
-    DexterSetup \
-    DexterUpdater
-
-# Resource overlays replace customer-facing Android labels/branding.
 PRODUCT_PACKAGE_OVERLAYS += vendor/dexter/overlay
 
-# Production handset products must add their own telephony/camera/audio packages
-# and vendor HALs. This generic product deliberately does not fake hardware support.
+# This generic target intentionally omits device HALs/vendor images.
+# It must never be labelled as a retail flash image.
