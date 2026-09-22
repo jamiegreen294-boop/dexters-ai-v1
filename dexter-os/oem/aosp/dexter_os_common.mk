239f0775b9689ae88f07d9839865f8c901f9cb68
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-22 16:02:43 +0100

    Wire complete Dexter system app set

--- dexter-os/oem/aosp/dexter_os_common.mk
+++ dexter-os/oem/aosp/dexter_os_common.mk
@@ -4,11 +4,15 @@ PRODUCT_PACKAGES += \
     DexterDialer \
     DexterMessaging \
     DexterFiles \
-    DexterEsim
+    DexterEsim \
+    DexterSetup \
+    DexterUpdater \
+    DexterCamera
 
 PRODUCT_COPY_FILES += \
     vendor/dexter/permissions/privapp-permissions-dexter.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-dexter.xml \
-    vendor/dexter/permissions/default-permissions-dexter.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions-dexter.xml
+    vendor/dexter/permissions/default-permissions-dexter.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions-dexter.xml \
+    vendor/dexter/sysconfig/dexter-system-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/dexter-system-whitelist.xml
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.dexter.os=true \
