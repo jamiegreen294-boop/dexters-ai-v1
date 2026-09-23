4423464b57e855c08c736c924ea469498c8bfe0f
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-23 14:06:10 +0100

    Add Dexter AI mobile service worker

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='dexter-ai-test-v1';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/manifest.webmanifest','/dexter-icon.svg']).catch(()=>{})))});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
